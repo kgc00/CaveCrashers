49f35d3f546afd2c04b1bbff84cf41d7ed12ad6e
kgc00 <kirbygc00@gmail.com>
2020-11-13 22:07:39 -0800

    docs

--- CC/Gameplay/src/Helpers/GameplayHelpers.cs
+++ CC/Gameplay/src/Helpers/GameplayHelpers.cs
@@ -8,6 +8,12 @@ namespace CC.Gameplay.Helpers {
             return IsOutOfBounds(desiredPosition, bounds);
         }
 
+        /// <summary>
+        ///  Determins if move is OOB. Implementation relies on node positions being (0,0) or greater (e.g. no negatives).
+        /// </summary>
+        /// <param name="desiredPosition"></param>
+        /// <param name="bounds"></param>
+        /// <returns></returns>
         public static bool IsOutOfBounds(Vector2 desiredPosition, int[,] bounds) {
             if (desiredPosition.x >= bounds.GetLength(0) || desiredPosition.x < 0) return true;
             if (desiredPosition.y >= bounds.GetLength(1)|| desiredPosition.y < 0) return true;
